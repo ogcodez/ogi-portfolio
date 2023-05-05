f1f4387f51fbabd10f9eac997056cf8b302cd65d
Ognjen Trajkovic <trcognjen@gmail.com>
2023-05-05 12:35:54 +0200

    final

--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -6,7 +6,7 @@ export const info = {
   position: 'a Full Stack Developer',
   gradient: `-webkit-linear-gradient(135deg, ${['rgb(0,255,164)', 'rgb(166,104,255)']})`, // don't change this either
   baseColor: 'rgb(13, 223, 247)',
-  bio: "Hello! I'm Trajkovic Ognjen. I'm a Web Developer. I'm a graduate student of Software Engineering at FIT - Metropolitan University. I enjoy building web applications and learning new technologies. I'm currently job hunting. I'm also a musician and I love to travel and explore new places. I'm currently based in Belgrade, Serbia.",
+  bio: "Hello! I'm Ognjen, a graduate student of Software Engineering at FIT - Metropolitan University. I enjoy building web apps and learning new things! I'm also a musician, photographer and I love to travel and explore new places. Currently based in Belgrade, Serbia.",
   skills: {
     languages: ['TypeScript', 'JavaScript', 'Java', 'C#', 'C/C++', 'HTML/CSS'],
     frameworks: [
